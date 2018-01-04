da185926d2c86446048eab89124e5a68cda55e25
tom <tom@mdashx.com>
2018-01-04 15:29:28 -0600

    Add 'lg' git alias
    Shows oneline log for ten commits with no pager.

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,2 +1,3 @@
 ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
-ln -s $HOME/dotfiles/.git-completion.bash $HOME/.git-completion.bash 
+ln -s $HOME/dotfiles/.git-completion.bash $HOME/.git-completion.bash
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
