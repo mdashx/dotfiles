e9fe95a9901a6428ec00f2ac4f68baae159faa57
tom <tom@mdashx.com>
2018-02-05 19:17:43 +0000

    Add tmux conf and use Python script for linking files

--- link_dotfiles.sh
+++
@@ -1,3 +0,0 @@
-ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
-ln -s $HOME/dotfiles/.git-completion.bash $HOME/.git-completion.bash
-ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
---
+++ link_files.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+filenames = [
+    '.bashrc',
+    '.git-completion.bash',
+    '.gitconfig',
+    '.tmux.conf',
+]
+
+for filename in filenames:
+    source_path = os.path.join(os.getcwd(), filename)
+    try:
+        target_path = os.environ['HOME']
+    except:
+        print('$HOME env var not set.')
+        raise SystemExit()
+    if os.path.isfile(source_path):
+        print('File exists: {}'.format(filename))
+    else:
+        subprocess.call(['ln', '-s', source_path, target_path])
+
