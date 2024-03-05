e02b5b6984db03867cf63bce6802e9bf14e3cd78
tom <tom@mdashx.com>
2024-03-05 11:42:09 -0600

    auto format on save all LSPs

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,6 +2,7 @@ local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
+  lsp.buffer_autoformat()
 end)
 
 lsp.ensure_installed({
